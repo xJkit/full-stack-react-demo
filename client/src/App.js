eaab5781f2f7a5bd571b3ff1686d29f959eb81ee
xJkit <xjkit54780@gmail.com>
2019-08-27 01:57:10 +0800

    Add bootstrap to the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 1,
+  }
+  render() {
+    return (
+      <div>
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">Active</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#" tabIndex="-1">Disabled</a>
+          </li>
+        </ul>
+        <div className="jumbotron">
+          <h1 className="display-4">{`Hello, world! ${this.state.count}`}</h1>
+          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-4" />
+          <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
+          <a
+            className="btn btn-primary btn-lg"
+            role="button"
+            onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+          >Learn more</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
